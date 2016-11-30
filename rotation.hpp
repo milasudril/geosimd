d00fdf6ee4a8f3e6ed72b38dd24c10b9d262f418
Torbjörn Rathsman <milasudril@gmail.com>
2016-11-30 13:50:13 +0100

    More efficient ways of creating rotation matrices

--- rotation.hpp
+++ rotation.hpp
@@ -8,12 +8,36 @@
 
 namespace GeoSIMD
 	{
+	struct X
+		{};
+
+	struct Y
+		{};
+
+	struct Z
+		{};
+
 	template<class T>
 	class Rotation
 		{
 		public:
-			constexpr explicit Rotation():
+			constexpr Rotation():
 				m_data(mat4_t<T>::identity())
+				{}
+
+			template<class U>
+			constexpr Rotation(Angle<U> angle,X) noexcept:
+				m_data(init_x(angle))
+				{}
+
+			template<class U>
+			constexpr Rotation(Angle<U> angle,Y) noexcept:
+				m_data(init_y(angle))
+				{}
+
+			template<class U>
+			constexpr Rotation(Angle<U> angle,Z) noexcept:
+				m_data(init_z(angle))
 				{}
 
 			constexpr const mat4_t<T>& data() const noexcept
@@ -22,33 +46,21 @@ namespace GeoSIMD
 			template<class U>
 			Rotation& rotateX(Angle<U> angle) noexcept
 				{
-				mat4_t<T> R(mat4_t<T>::identity());
-				R.column(0)=GeoSIMD::vec4_t<float>{one<T>(),zero<T>(),zero<T>(),zero<T>()};
-				R.column(1)=GeoSIMD::vec4_t<float>{zero<T>(),cos(angle),sin(angle),zero<T>()};
-				R.column(2)=GeoSIMD::vec4_t<float>{zero<T>(),-sin(angle),cos(angle),zero<T>()};
-				m_data*=R;
+				m_data*=init_x(angle);
 				return *this;
 				}
 
 			template<class U>
 			Rotation& rotateY(Angle<U> angle) noexcept
 				{
-				mat4_t<T> R(mat4_t<T>::identity());
-				R.column(0)=GeoSIMD::vec4_t<float>{cos(angle),zero<T>(),-sin(angle),zero<T>()};
-				R.column(1)=GeoSIMD::vec4_t<float>{zero<T>(),one<T>(),zero<T>(),zero<T>()};
-				R.column(2)=GeoSIMD::vec4_t<float>{sin(angle),zero<T>(),cos(angle),zero<T>()};
-				m_data*=R;
+				m_data*=init_y(angle);
 				return *this;
 				}
 
 			template<class U>
 			Rotation& rotateZ(Angle<U> angle) noexcept
 				{
-				mat4_t<T> R(mat4_t<T>::identity());
-				R.column(0)=GeoSIMD::vec4_t<float>{cos(angle),sin(angle),zero<T>(),zero<T>()};
-				R.column(1)=GeoSIMD::vec4_t<float>{-sin(angle),cos(angle),zero<T>(),zero<T>()};
-				R.column(2)=GeoSIMD::vec4_t<float>{zero<T>(),zero<T>(),one<T>(),zero<T>()};
-				m_data*=R;
+				m_data*=init_z(angle);
 				return *this;
 				}
 
@@ -57,14 +69,44 @@ namespace GeoSIMD
 
 			template<class U>
 			static Rotation xyz(Angle<U> x,Angle<U> y,Angle<U> z) noexcept
-				{return Rotation().rotateX(x).rotateY(y).rotateZ(z);}
+				{return Rotation(x,X()).rotateY(y).rotateZ(z);}
 
 			template<class U>
 			static Rotation zyz(Angle<U> z,Angle<U> y,Angle<U> z_prime) noexcept
-				{return Rotation().rotateZ(z).rotateY(y).rotateZ(z_prime);}
+				{return Rotation(z,Z()).rotateY(y).rotateZ(z_prime);}
 
 		private:
 			mat4_t<T> m_data;
+
+			template<class U>
+			static mat4_t<T> init_x(Angle<U> angle)
+				{
+				mat4_t<T> R(mat4_t<T>::identity());
+				R.column(0)=GeoSIMD::vec4_t<float>{one<T>(),zero<T>(),zero<T>(),zero<T>()};
+				R.column(1)=GeoSIMD::vec4_t<float>{zero<T>(),cos(angle),sin(angle),zero<T>()};
+				R.column(2)=GeoSIMD::vec4_t<float>{zero<T>(),-sin(angle),cos(angle),zero<T>()};
+				return R;
+				}
+
+			template<class U>
+			static mat4_t<T> init_y(Angle<U> angle)
+				{
+				mat4_t<T> R(mat4_t<T>::identity());
+				R.column(0)=GeoSIMD::vec4_t<float>{cos(angle),zero<T>(),-sin(angle),zero<T>()};
+				R.column(1)=GeoSIMD::vec4_t<float>{zero<T>(),one<T>(),zero<T>(),zero<T>()};
+				R.column(2)=GeoSIMD::vec4_t<float>{sin(angle),zero<T>(),cos(angle),zero<T>()};
+				return R;
+				}
+
+			template<class U>
+			static mat4_t<T> init_z(Angle<U> angle)
+				{
+				mat4_t<T> R(mat4_t<T>::identity());
+				R.column(0)=GeoSIMD::vec4_t<float>{cos(angle),sin(angle),zero<T>(),zero<T>()};
+				R.column(1)=GeoSIMD::vec4_t<float>{-sin(angle),cos(angle),zero<T>(),zero<T>()};
+				R.column(2)=GeoSIMD::vec4_t<float>{zero<T>(),zero<T>(),one<T>(),zero<T>()};
+				return R;
+				}
 		};
 
 	template<class T,class U>
@@ -73,7 +115,7 @@ namespace GeoSIMD
 
 	template<class T,class U>
 	Rotation<T> rotateX(Angle<U> angle) noexcept
-		{return Rotation<T>().rotateX(angle);}
+		{return Rotation<T>(angle,X());}
 
 	template<class T,class U>
 	Rotation<T> rotateY(Rotation<T> R,Angle<U> angle) noexcept
@@ -81,7 +123,7 @@ namespace GeoSIMD
 
 	template<class T,class U>
 	Rotation<T> rotateY(Angle<U> angle) noexcept
-		{return Rotation<T>().rotateY(angle);}
+		{return Rotation<T>(angle,Y());}
 
 	template<class T,class U>
 	Rotation<T> rotateZ(Rotation<T> R,Angle<U> angle) noexcept
@@ -89,7 +131,7 @@ namespace GeoSIMD
 
 	template<class T,class U>
 	Rotation<T> rotateZ(Angle<U> angle) noexcept
-		{return Rotation<T>().rotateZ(angle);}
+		{return Rotation<T>(angle,Z());}
 	}
 
 #endif
