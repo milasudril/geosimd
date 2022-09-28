1020e842641b20cb67dae76ed5db5e807bcaeb13
Torbjörn Rathsman <milasudril@gmail.com>
2022-09-28 21:43:16 +0200

    Add more restrictions to affine_space

--- affine_space.hpp
+++ affine_space.hpp
@@ -7,10 +7,19 @@
 namespace geosimd
 {
 	template<class T>
-	concept affine_space = requires(T a)
+	concept affine_space = requires(T)
 	{
 		typename T::vector_type;
 		typename T::scalar_type;
+
+		{ std::declval<typename T::vector_type>() - std::declval<typename T::vector_type>() } -> std::same_as<typename T::vector_type>;
+
+		{ std::declval<typename T::vector_type>() + std::declval<typename T::vector_type>() } -> std::same_as<typename T::vector_type>;
+
+		{ std::declval<typename T::vector_type&>() -= std::declval<typename T::vector_type>() } -> std::same_as<typename T::vector_type&>;
+
+		{ std::declval<typename T::vector_type&>() += std::declval<typename T::vector_type>() } -> std::same_as<typename T::vector_type&>;
+
 		{ T::dimension_count() } -> std::same_as<size_t>;
 		typename std::integral_constant<size_t, T::dimension_count()>;
 	};
