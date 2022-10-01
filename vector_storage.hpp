6fbd5e502f51141ebbc8aa2d02e6935b45d3abd5
Torbjörn Rathsman <milasudril@gmail.com>
2022-10-01 15:02:02 +0200

    Introduce vector storage types

--- vector_storage.hpp
+++ vector_storage.hpp
@@ -4,6 +4,7 @@
 #include <bit>
 #include <cstddef>
 #include <cstdint>
+#include <array>
 
 namespace geosimd
 {
@@ -87,6 +88,26 @@ namespace geosimd
 				&& can_vectorize<T>();
 		}
 	}
+
+	template<class T, size_t N>
+	class emulated_vector
+	{
+	public:
+	private:
+		std::array<T, N> m_data;
+	};
+
+	template<class T, size_t N>
+	using native_vector [[gnu::vector_size(sizeof(T)*N)]] = T;
+
+	template<class T, size_t N>
+	struct vector_storage
+	{
+		using type = std::conditional_t<can_vectorize<T>(N), native_vector<T, N>, emulated_vector<T, N>>;
+	};
+
+	template<class T, size_t N>
+	using vec_t = vector_storage<T, N>::type;
 };
 
 #endif
--- vector_storage.test.cpp
+++ vector_storage.test.cpp
@@ -6,6 +6,8 @@
 
 namespace testcases
 {
+	// This should be true on any x86-64 system
+
 	static_assert(geosimd::vector_limits::can_vectorize<float>(4));
 	static_assert(geosimd::vector_limits::can_vectorize<double>(2));
 	static_assert(!geosimd::vector_limits::can_vectorize<float>(2));
