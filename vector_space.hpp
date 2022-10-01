6534e76f33731eb57a095b4efe5525bd973b7082
Torbjörn Rathsman <milasudril@gmail.com>
2022-10-01 11:40:55 +0200

    Clean-up

--- vector_space.hpp
+++ vector_space.hpp
@@ -111,50 +111,5 @@ namespace geosimd
 	{
 		{ norm(std::declval<typename T::vector_type>()) } -> std::totally_ordered;
 	};
-
-
-#if 0
-
-
-	template<class T>
-	concept vector_space = requires(T)
-	{
-		typename T::vector_type;
-		typename T::scalar_type;
-
-		{ std::declval<typename T::vector_type>() - std::declval<typename T::vector_type>() }
-			-> std::same_as<typename T::vector_type>;
-
-		{ std::declval<typename T::vector_type>() + std::declval<typename T::vector_type>() }
-			-> std::same_as<typename T::vector_type>;
-
-		{ std::declval<typename T::vector_type&>() -= std::declval<typename T::vector_type>() }
-			-> std::same_as<typename T::vector_type&>;
-
-		{ std::declval<typename T::vector_type&>() += std::declval<typename T::vector_type>() }
-			-> std::same_as<typename T::vector_type&>;
-
-		{ std::declval<typename T::vector_type&>() *= std::declval<typename T::scalar_type>() }
-			-> std::same_as<typename T::vector_type&>;
-
-		{ std::declval<typename T::vector_type&>() /= std::declval<typename T::scalar_type>() }
-			-> std::same_as<typename T::vector_type&>;
-
-		{ std::declval<typename T::scalar_type>() * std::declval<typename T::vector_type>() }
-			-> std::same_as<typename T::vector_type>;
-
-		{ std::declval<typename T::vector_type>() * std::declval<typename T::scalar_type>() }
-			-> std::same_as<typename T::vector_type>;
-
-		{ std::declval<typename T::vector_type>() / std::declval<typename T::scalar_type>() }
-			-> std::same_as<typename T::vector_type>;
-
-		{ typename T::vector_type{zero{}} } -> std::same_as<typename T::vector_type>;
-
-		{ T::dimension_count() } -> std::same_as<size_t>;
-
-		typename std::integral_constant<size_t, T::dimension_count()>;
-	};
-#endif
 }
 #endif
