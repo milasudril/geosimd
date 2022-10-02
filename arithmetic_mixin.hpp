47c913adc9cc99d92872470ca003e08bd4bc2547
Torbjörn Rathsman <milasudril@gmail.com>
2022-10-02 08:28:40 +0200

    Move arithmetic_mixin to separate file

---
+++ arithmetic_mixin.hpp
@@ -0,0 +1,89 @@
+#ifndef ARITHMETIC_MIXIN_HPP
+#define ARITHMETIC_MIXIN_HPP
+
+#include "./inline.hpp"
+
+namespace geosimd
+{
+	template<class Derived, class ScalarType>
+	struct arithmetic_mixin
+	{
+		using scalar_type = ScalarType;
+
+		GEOSIMD_FULL_INLINE constexpr Derived& operator*=(Derived other)
+		{
+			auto& derived = static_cast<Derived&>(*this);
+			derived.get() *= other.get();
+			return derived;
+		}
+
+		GEOSIMD_FULL_INLINE constexpr Derived& operator/=(Derived other)
+		{
+			auto& derived = static_cast<Derived&>(*this);
+			derived.get() /= other.get();
+			return derived;
+		}
+
+		GEOSIMD_FULL_INLINE constexpr Derived& operator+=(Derived other)
+		{
+			auto& derived = static_cast<Derived&>(*this);
+			derived.get() += other.get();
+			return derived;
+		}
+
+		GEOSIMD_FULL_INLINE constexpr Derived& operator-=(Derived other)
+		{
+			auto& derived = static_cast<Derived&>(*this);
+			derived.get() -= other.get();
+			return derived;
+		}
+
+		GEOSIMD_FULL_INLINE constexpr Derived& operator*=(scalar_type other)
+		{
+			auto& derived = static_cast<Derived&>(*this);
+			derived.get() *= other;
+			return derived;
+		}
+
+		GEOSIMD_FULL_INLINE constexpr Derived& operator/=(scalar_type other)
+		{
+			auto& derived = static_cast<Derived&>(*this);
+			derived.get() /= other;
+			return derived;
+		}
+
+		GEOSIMD_FLATTEN constexpr bool operator==(Derived other) const
+		{
+			auto const diff = static_cast<Derived const&>(*this) - other;
+			for(size_t k = 0; k != std::size(diff); ++k)
+			{
+				if(diff[k] != scalar_type{})
+				{ return false; }
+			}
+			return true;
+		}
+
+		GEOSIMD_INLINE constexpr bool operator!=(Derived other) const
+		{
+			return !(*this == other);
+		}
+	};
+
+	template<class Op1, class Op2>
+	GEOSIMD_FULL_INLINE constexpr auto operator*(Op1 a, Op2 b)
+	{ return a *= b; }
+
+	template<class Op1, class Op2>
+	GEOSIMD_FULL_INLINE constexpr auto operator/(Op1 a, Op2 b)
+	{ return a /= b; }
+
+	template<class Op1, class Op2>
+	GEOSIMD_FULL_INLINE constexpr auto operator+(Op1 a, Op2 b)
+	{ return a += b; }
+
+	template<class Op1, class Op2>
+	GEOSIMD_FULL_INLINE constexpr auto operator-(Op1 a, Op2 b)
+	{ return a -= b; }
+}
+
+#endif
--- vec_t.hpp
+++ vec_t.hpp
@@ -2,90 +2,10 @@
 #define GEOSIMD_VECT_HPP
 
 #include "./vector_storage.hpp"
-#include "./inline.hpp"
+#include "./arithmetic_mixin.hpp"
 
 namespace geosimd
 {
-	template<class Derived, class ScalarType>
-	struct arithmetic_mixin
-	{
-		using scalar_type = ScalarType;
-
-		GEOSIMD_FULL_INLINE constexpr Derived& operator*=(Derived other)
-		{
-			auto& derived = static_cast<Derived&>(*this);
-			derived.get() *= other.get();
-			return derived;
-		}
-
-		GEOSIMD_FULL_INLINE constexpr Derived& operator/=(Derived other)
-		{
-			auto& derived = static_cast<Derived&>(*this);
-			derived.get() /= other.get();
-			return derived;
-		}
-
-		GEOSIMD_FULL_INLINE constexpr Derived& operator+=(Derived other)
-		{
-			auto& derived = static_cast<Derived&>(*this);
-			derived.get() += other.get();
-			return derived;
-		}
-
-		GEOSIMD_FULL_INLINE constexpr Derived& operator-=(Derived other)
-		{
-			auto& derived = static_cast<Derived&>(*this);
-			derived.get() -= other.get();
-			return derived;
-		}
-
-		GEOSIMD_FULL_INLINE constexpr Derived& operator*=(scalar_type other)
-		{
-			auto& derived = static_cast<Derived&>(*this);
-			derived.get() *= other;
-			return derived;
-		}
-
-		GEOSIMD_FULL_INLINE constexpr Derived& operator/=(scalar_type other)
-		{
-			auto& derived = static_cast<Derived&>(*this);
-			derived.get() /= other;
-			return derived;
-		}
-
-		GEOSIMD_FLATTEN constexpr bool operator==(Derived other) const
-		{
-			auto const diff = static_cast<Derived const&>(*this) - other;
-			for(size_t k = 0; k != std::size(diff); ++k)
-			{
-				if(diff[k] != scalar_type{})
-				{ return false; }
-			}
-			return true;
-		}
-
-		GEOSIMD_INLINE constexpr bool operator!=(Derived other) const
-		{
-			return !(*this == other);
-		}
-	};
-
-	template<class Op1, class Op2>
-	GEOSIMD_FULL_INLINE constexpr auto operator*(Op1 a, Op2 b)
-	{ return a *= b; }
-
-	template<class Op1, class Op2>
-	GEOSIMD_FULL_INLINE constexpr auto operator/(Op1 a, Op2 b)
-	{ return a /= b; }
-
-	template<class Op1, class Op2>
-	GEOSIMD_FULL_INLINE constexpr auto operator+(Op1 a, Op2 b)
-	{ return a += b; }
-
-	template<class Op1, class Op2>
-	GEOSIMD_FULL_INLINE constexpr auto operator-(Op1 a, Op2 b)
-	{ return a -= b; }
-
 	template<class T, size_t N>
 	class vec_t:public arithmetic_mixin<vec_t<T, N>, T>
 	{
