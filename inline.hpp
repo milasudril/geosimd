1929c0399509805abc0655214671e39d438652d9
Torbjörn Rathsman <milasudril@gmail.com>
2022-10-07 21:54:48 +0200

    Disable endbr64 for optimized functions

--- inline.hpp
+++ inline.hpp
@@ -1,7 +1,7 @@
 #ifndef GEOSMID_INLINE_HPP
 #define GEOSIMD_INLINE_OPT_HPP
 
-#define GEOSIMD_FORCE_OPT [[gnu::optimize(3)]]
+#define GEOSIMD_FORCE_OPT [[gnu::optimize(3)]] [[gnu::nocf_check]]
 #define GEOSIMD_INLINE [[gnu::always_inline]]
 #define GEOSIMD_FLATTEN [[gnu::flatten]]
 #define GEOSIMD_INLINE_OPT GEOSIMD_FORCE_OPT GEOSIMD_INLINE
