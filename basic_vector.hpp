2c4c3fbac15e289017ba100da7a4ffb81bce38a2
Torbjörn Rathsman <milasudril@gmail.com>
2022-10-07 20:08:45 +0200

    Make basic_vector work in more cases

--- basic_vector.hpp
+++ basic_vector.hpp
@@ -6,6 +6,15 @@
 
 namespace geosimd
 {
+	template<class V>
+	concept has_homogenous_coordinates = requires(V){ typename V::enable_homogenous_coordinates_t; };
+
+	template<class VectorType>
+	concept has_subscript_operator = requires(VectorType a)
+	{
+		{ a[std::declval<size_t>()] };
+	};
+
 	template<vector_space V>
 	class basic_vector:public vectorops_mixin<basic_vector<V>, typename V::scalar_type>
 	{
@@ -20,12 +29,25 @@ namespace geosimd
 
 		template<class ... Args>
 		requires std::conjunction_v<std::is_same<scalar_type, Args>...>
+			&& (!has_homogenous_coordinates<V>)
+		GEOSIMD_INLINE_OPT constexpr explicit basic_vector(scalar_type x, Args ... xn):
+			m_value{x, xn...}
+		{}
+
+		template<class ... Args>
+		requires std::conjunction_v<std::is_same<scalar_type, Args>...>
+			&& (has_homogenous_coordinates<V>)
 		GEOSIMD_INLINE_OPT constexpr explicit basic_vector(scalar_type x, Args ... xn):
 			m_value{x, xn..., scalar_type{}}
 		{}
 
 		GEOSIMD_INLINE_OPT constexpr scalar_type operator[](size_t n) const
-		{ return m_value[n]; }
+		{
+			if constexpr(has_subscript_operator<storage_type>)
+			{ return m_value[n]; }
+			else
+			{ return m_value; }
+		}
 
 	private:
 		storage_type m_value;
--- basic_vector.test.cpp
+++ basic_vector.test.cpp
@@ -8,28 +8,44 @@
 
 namespace geosimd_test
 {
-	struct my_vector_space
+	struct my_vector_space_3d
 	{
 		using vector_type = geosimd::vec4i32_t;
 		using scalar_type = int;
+		using enable_homogenous_coordinates_t = void;
 	};
 
-	static_assert(geosimd::vector_space<my_vector_space>);
-	static_assert(geosimd::vector<geosimd::basic_vector<my_vector_space>, int>);
+	static_assert(geosimd::vector_space<my_vector_space_3d>);
+	static_assert(geosimd::vector<geosimd::basic_vector<my_vector_space_3d>, int>);
+
+	struct my_vector_space_1d
+	{
+		using vector_type = intptr_t;
+		using scalar_type = intptr_t;
+		using point_type = std::byte*;
+	};
 }
 
-TESTCASE(geosimd_basic_vector_construct)
+TESTCASE(geosimd_basic_vector_3d_construct)
 {
-	constexpr geosimd::basic_vector<geosimd_test::my_vector_space> a{};
+	constexpr geosimd::basic_vector<geosimd_test::my_vector_space_3d> a{};
 	EXPECT_EQ(a[0], 0);
 	EXPECT_EQ(a[1], 0);
 	EXPECT_EQ(a[2], 0);
 	EXPECT_EQ(a[3], 0);
 
-	constexpr geosimd::basic_vector<geosimd_test::my_vector_space> b{1, 2, 3};
+	constexpr geosimd::basic_vector<geosimd_test::my_vector_space_3d> b{1, 2, 3};
 	EXPECT_EQ(b[0], 1);
 	EXPECT_EQ(b[1], 2);
 	EXPECT_EQ(b[2], 3);
 	EXPECT_EQ(b[3], 0);
+}
 
+TESTCASE(geosimd_basic_vector_1d_construct)
+{
+	constexpr geosimd::basic_vector<geosimd_test::my_vector_space_1d> a{};
+	EXPECT_EQ(a[0], 0);
+
+	constexpr geosimd::basic_vector<geosimd_test::my_vector_space_1d> b{1};
+	EXPECT_EQ(b[0], 1);
 }
