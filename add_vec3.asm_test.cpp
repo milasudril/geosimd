87a3adfadfecd505abc95375be8db23dc74efd6b
Torbjörn Rathsman <milasudril@gmail.com>
2022-10-07 21:39:22 +0200

    Add testcase for optimization

---
+++ add_vec3.asm_test.cpp
@@ -0,0 +1,28 @@
+#include "./basic_vector.hpp"
+#include "./vec_t.hpp"
+
+namespace geosimd_test
+{
+	struct my_vector_space_3d
+	{
+		using vector_type = geosimd::vec4i32_t;
+		using scalar_type = int;
+		using enable_homogenous_coordinates_t = void;
+	};
+
+	using vec3 = geosimd::basic_vector<my_vector_space_3d>;
+
+	static_assert(sizeof(vec3) == 16);
+
+	using vec4_raw = geosimd::native_vector<int, 4>;
+
+	geosimd::vec4i32_t add(geosimd::vec4i32_t a, geosimd::vec4i32_t b)
+	{ return a + b; }
+
+	vec3 add(vec3 a, vec3 b)
+	{ return a + b; }
+
+	vec4_raw add(vec4_raw a, vec4_raw b)
+	{ return a + b; }
+}
+
