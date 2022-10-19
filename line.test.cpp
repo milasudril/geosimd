ef595f2e54968940de9116a2b4731430a14193ea
Torbjörn Rathsman <milasudril@gmail.com>
2022-10-19 18:15:40 +0200

    Validate locations

--- line.test.cpp
+++ line.test.cpp
@@ -9,6 +9,7 @@ TESTCASE(geodimd_line)
 {
 	using loc = geosimd::location<float, 3>;
 
-	geosimd::line line{loc{1.0f, 2.0f, 3.0f}, loc{2.0f, 2.0f, 3.0f}};
-	(void)line;
+	constexpr geosimd::line line{loc{1.0f, 2.0f, 3.0f}, loc{2.0f, 4.0f, 6.0f}};
+	EXPECT_EQ(line.p1, (loc{1.0f, 2.0f, 3.0f}));
+	EXPECT_EQ(line.p2, (loc{2.0f, 4.0f, 6.0f}));
 }
