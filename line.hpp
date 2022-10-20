c1f6dd9844069c305f2df434e4a627627290353c
Torbjörn Rathsman <milasudril@gmail.com>
2022-10-20 20:53:47 +0200

    Add function to compute line-line interection

--- line.hpp
+++ line.hpp
@@ -3,6 +3,7 @@
 
 #include "./abstract_spaces.hpp"
 #include "./basic_point.hpp"
+#include "./hilbert_space.hpp"
 
 namespace geosimd
 {
@@ -12,6 +13,35 @@ namespace geosimd
 		basic_point<V> p1;
 		basic_point<V> p2;
 	};
+
+	template<class T>
+	struct line_intersection
+	{
+		T a;
+		T b;
+	};
+
+	template<hilbert_space V>
+	constexpr auto intersection(line<V> const& a, line<V> const& b)
+	{
+		auto const d_a = a.p2 - a.p1;
+		auto const d_b = b.p2 - b.p1;
+		auto const d_o = a.p1 - b.p1;
+
+		auto const a11 = inner_product(d_a, d_a);
+		auto const a12 = -inner_product(d_a, d_b);
+		auto const a21 = -a12;
+		auto const a22 = -inner_product(d_b, d_b);
+
+		auto const rhs_a = -inner_product(d_a, d_o);
+		auto const rhs_b = -inner_product(d_b, d_o);
+
+		auto const denom = a11*a22 - a12*a21;
+		auto const s =  (a22*rhs_a - a12*rhs_b)/denom;
+		auto const t = -(a21*rhs_a - a11*rhs_b)/denom;
+		return line_intersection{s, t};
+
+	}
 }
 
 #endif
