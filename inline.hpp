21524984cc3fdce22c5e5be0ad667fe412d412f3
Torbjörn Rathsman <milasudril@gmail.com>
2022-11-03 20:59:46 +0100

    Fix broken include guard

--- inline.hpp
+++ inline.hpp
@@ -1,5 +1,5 @@
-#ifndef GEOSMID_INLINE_HPP
-#define GEOSIMD_INLINE_OPT_HPP
+#ifndef GEOSIMD_INLINE_HPP
+#define GEOSIMD_INLINE_HPP
 
 #define GEOSIMD_FORCE_OPT [[gnu::optimize(3)]] [[gnu::nocf_check]]
 #define GEOSIMD_INLINE [[gnu::always_inline]]
