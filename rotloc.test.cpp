0eeee5a6d62df6bdf5d15eec2d54dea3414f4276
Torbjörn Rathsman <milasudril@gmail.com>
2022-11-13 14:42:40 +0100

    Test to apply rotloc to point

--- rotloc.test.cpp
+++ rotloc.test.cpp
@@ -1,6 +1,7 @@
 //@	{"target":{"name":"rocloc.test"}}
 
 #include "./rotloc.hpp"
+#include "./basic_point.hpp"
 
 #include "testfwk/testfwk.hpp"
 
@@ -41,4 +42,20 @@ TESTCASE(geosimd_rotloc_extract_parts)
 	geosimd::rotloc const A{T, R};
 	EXPECT_EQ(A.translation_part(), T.offset());
 	EXPECT_EQ(A.rotation_part(), R.get());
+}
+
+TESTCASE(geosimd_rotloc_apply)
+{
+	geosimd::basic_vector<my_vector_space> const offset{1.0f, 2.0f, 3.0f};
+	geosimd::translation const T{offset};
+	geosimd::rotation const R{
+		geosimd::rotation<my_vector_space>{geosimd::rotation_angle{geosimd::turns{0.25f}},
+		geosimd::dimension_tag<2>{}
+	}};
+
+	geosimd::rotloc const A{T, R};
+
+	geosimd::basic_point<my_vector_space> loc{1.0f, 2.0f, 3.0f};
+
+	EXPECT_EQ(A.get() * loc.get(), (geosimd::vec_t<float, 4>{-1.0f, 3.0f, 6.0f, 1.0f}));
 }
