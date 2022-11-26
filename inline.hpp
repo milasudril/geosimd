09999d8b2b72b1fee1ec18a1828b1db8ad4d49d0
Torbjörn Rathsman <milasudril@gmail.com>
2022-11-26 12:03:11 +0100

    Turn of forced optimization when generating code coverage

--- inline.hpp
+++ inline.hpp
@@ -1,9 +1,16 @@
 #ifndef GEOSIMD_INLINE_HPP
 #define GEOSIMD_INLINE_HPP
 
-#define GEOSIMD_FORCE_OPT [[gnu::optimize(3)]] [[gnu::nocf_check]]
-#define GEOSIMD_INLINE [[gnu::always_inline]]
-#define GEOSIMD_FLATTEN [[gnu::flatten]]
-#define GEOSIMD_INLINE_OPT GEOSIMD_FORCE_OPT GEOSIMD_INLINE
+#ifdef COVERAGE_BUILD
+	#define GEOSIMD_FORCE_OPT
+	#define GEOSIMD_INLINE
+	#define GEOSIMD_FLATTEN
+	#define GEOSIMD_INLINE_OPT
+#else
+	#define GEOSIMD_FORCE_OPT [[gnu::optimize(3)]] [[gnu::nocf_check]]
+	#define GEOSIMD_INLINE [[gnu::always_inline]]
+	#define GEOSIMD_FLATTEN [[gnu::flatten]]
+	#define GEOSIMD_INLINE_OPT GEOSIMD_FORCE_OPT GEOSIMD_INLINE
+#endif
 
 #endif
