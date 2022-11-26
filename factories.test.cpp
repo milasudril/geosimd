834a4958c46ab01dcf7582c5e65ddf3f380b3e9d
Torbjörn Rathsman <milasudril@gmail.com>
2022-11-26 11:50:46 +0100

    Improve code coverage

--- factories.test.cpp
+++ factories.test.cpp
@@ -4,18 +4,36 @@
 
 #include "testfwk/testfwk.hpp"
 
+int (* one_ptr)();
+
 TESTCASE(factories_one)
 {
 	auto one = geosimd::one<int>();
 	static_assert(std::is_same_v<decltype(one), int>);
 	EXPECT_EQ(one, 1);
+
+	if(!std::is_constant_evaluated())
+	{
+		// Do an indirect call through a global pointer to collect code coverage
+		one_ptr = geosimd::one<int>;
+		EXPECT_EQ(one_ptr(), 1);
+	}
 }
+
+int (* zero_ptr)();
 
 TESTCASE(factories_zero)
 {
-	auto one = geosimd::zero<int>();
-	static_assert(std::is_same_v<decltype(one), int>);
-	EXPECT_EQ(one, 0);
+	auto zero = geosimd::zero<int>();
+	static_assert(std::is_same_v<decltype(zero), int>);
+	EXPECT_EQ(zero, 0);
+
+	if(!std::is_constant_evaluated())
+	{
+		// Do an indirect call through a global pointer to collect code coverage
+		zero_ptr = geosimd::zero<int>;
+		EXPECT_EQ(zero_ptr(), 0);
+	}
 }
 
 TESTCASE(factories_create)
