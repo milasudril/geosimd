401bb5c6f9898ddb6dc5070a900318b2298308a0
Torbjörn Ratshman <milasudril@gmail.com>
2022-12-27 16:58:03 +0100

    Model vec_container after STL adapters

--- vec_container.hpp
+++ vec_container.hpp
@@ -1,12 +1,16 @@
 #ifndef GEOSIMD_VEC_CONTAINER_HPP
 #define GEOSIMD_VEC_CONTAINER_HPP
 
+#include "./abstract_spaces.hpp"
+
 #include <algorithm>
+#include <vector>
 #include <cassert>
 
 namespace geosimd
 {
-	template<class Container>
+	template<vector V, class Container = std::vector<V>>
+	requires requires(){typename V::scalar_type;}
 	class vec_container:private Container
 	{
 	public:
@@ -17,7 +21,8 @@ namespace geosimd
 		using Container::size;
 
 		using value_type = typename Container::value_type;
-		
+		using container_type = Container;
+
 		template<class Dummy = void>
 		requires requires(Container x) {
 			{x.push_back(value_type{})};
@@ -26,35 +31,35 @@ namespace geosimd
 		{
 			return Container::push_back(val);
 		}
-		
+
 		template<class Dummy = void>
 		requires(std::ranges::random_access_range<Container>)
 		value_type& operator[](size_t n)
 		{
 			return *(begin() + n);
 		}
-		
+
 		template<class Dummy = void>
 		requires(std::ranges::random_access_range<Container>)
 		value_type operator[](size_t n) const
 		{
 			return *(begin() + n);
 		}
-		
+
 		template<std::ranges::input_range Other>
 		requires(std::is_same_v<std::ranges::range_value_t<Other>, value_type>)
 		constexpr bool operator==(Other const& other) const
 		{
-			return std::ranges::equal(*this, other); 
+			return std::ranges::equal(*this, other);
 		}
-		
+
 		template<std::ranges::input_range Other>
 		requires(std::is_same_v<std::ranges::range_value_t<Other>, value_type>)
 		constexpr bool operator!=(vec_container const& other) const
 		{
 			return !(*this == other);
 		}
-		
+
 		template<std::ranges::sized_range Other>
 		requires(std::is_same_v<std::ranges::range_value_t<Other>, value_type>)
 		constexpr vec_container& operator+=(Other const& other)
@@ -64,18 +69,18 @@ namespace geosimd
 			auto i_this = begin();
 			auto i_other = other.begin();
 			auto const this_end = end();
-			
+
 			while(i_this != this_end)
 			{
 				*i_this += *i_other;
-				
+
 				++i_this;
 				++i_other;
 			}
-			
+
 			return *this;
 		}
-		
+
 		template<std::ranges::sized_range Other>
 		requires(std::is_same_v<std::ranges::range_value_t<Other>, value_type>)
 		constexpr vec_container& operator-=(Other const& other)
@@ -85,15 +90,15 @@ namespace geosimd
 			auto i_this = begin();
 			auto i_other = other.begin();
 			auto const this_end = end();
-			
+
 			while(i_this != this_end)
 			{
 				*i_this -= *i_other;
-				
+
 				++i_this;
 				++i_other;
 			}
-			
+
 			return *this;
 		}
 
@@ -122,40 +127,40 @@ namespace geosimd
 		}
 	};
 
-	template<class T, std::ranges::input_range Other>
-	requires(std::is_same_v<std::ranges::range_value_t<Other>, typename vec_container<T>::value_type>)
-	constexpr auto operator+(vec_container<T> a, Other const& b)
+	template<vector V, class Container, std::ranges::input_range Other>
+	requires(std::is_same_v<std::ranges::range_value_t<Other>, V>)
+	constexpr auto operator+(vec_container<V, Container> a, Other const& b)
 	{
 		return a += b;
 	}
-	
-	template<class T, std::ranges::input_range Other>
-	requires(std::is_same_v<std::ranges::range_value_t<Other>, typename vec_container<T>::value_type>)
-	constexpr auto operator-(vec_container<T> a, Other const& b)
+
+	template<vector V, class Container, std::ranges::input_range Other>
+	requires(std::is_same_v<std::ranges::range_value_t<Other>, V>)
+	constexpr auto operator-(vec_container<V, Container> a, Other const& b)
 	{
 		return a -= b;
 	}
-	
-	template<class T>
-	constexpr auto operator*(typename vec_container<T>::value_type::scalar_type b, vec_container<T> a)
+
+	template<vector V, class Container>
+	constexpr auto operator*(typename V::scalar_type b, vec_container<V, Container> a)
 	{
 		return a *= b;
 	}
-	
-	template<class T>
-	constexpr auto operator*(vec_container<T> a, typename vec_container<T>::value_type::scalar_type b)
+
+	template<vector V, class Container>
+	constexpr auto operator*(vec_container<V, Container> a, typename V::scalar_type b)
 	{
 		return a *= b;
 	}
-	
-	template<class T>
-	constexpr auto operator/(vec_container<T> a, typename vec_container<T>::value_type::scalar_type b)
+
+	template<vector V, class Container>
+	constexpr auto operator/(vec_container<V, Container> a, typename V::scalar_type b)
 	{
 		return a /= b;
 	}
-	
-	template<class T>
-	std::string to_string(vec_container<T> const& obj)
+
+	template<vector V, class Container>
+	std::string to_string(vec_container<V, Container> const& obj)
 	{
 		std::string ret{};
 		std::ranges::for_each(obj, [&ret](auto const& item){
--- vec_container.test.cpp
+++ vec_container.test.cpp
@@ -14,88 +14,88 @@ namespace
 		using scalar_type = int;
 		using vector_type = geosimd::vec_t<int, 4>;
 	};
-	
+
 	using my_vector = geosimd::basic_vector<my_vector_space>;
 }
 
 TESTCASE(geosimd_vec_container_add_sub)
 {
-	geosimd::vec_container<std::array<my_vector, 3>> a;
+	geosimd::vec_container<my_vector, std::array<my_vector, 3>> a;
 	a[0] = my_vector{1, 2, 3, 4};
 	a[1] = my_vector{5, 6, 7, 8};
 	a[2] = my_vector{9, 10, 11, 12};
-	
-	geosimd::vec_container<std::array<my_vector, 3>> b;
+
+	geosimd::vec_container<my_vector, std::array<my_vector, 3>> b;
 	b[0] = my_vector{12, 11, 10, 9};
 	b[1] = my_vector{8, 7, 6, 5};
 	b[2] = my_vector{4, 3, 2, 1};
-	
+
 	auto const old_a = a;
-	
+
 	a += b;
-	
-	geosimd::vec_container<std::array<my_vector, 3>> expected_sum;
+
+	geosimd::vec_container<my_vector, std::array<my_vector, 3>> expected_sum;
 	expected_sum[2] = my_vector{13, 13, 13, 13};
 	expected_sum[1] = my_vector{13, 13, 13, 13};
 	expected_sum[0] = my_vector{13, 13, 13, 13};
-	
+
 	EXPECT_EQ(a, expected_sum);
-	
+
 	a-=b;
-	
+
 	EXPECT_EQ(a, old_a);
-	
+
 	auto const c = a + b;
 	EXPECT_EQ(a, old_a);
 	EXPECT_EQ(c, expected_sum);
-	
+
 	auto const d = c - b;
 	EXPECT_EQ(d, old_a);
 }
 
 TESTCASE(geosimd_vec_container_add_other_container)
 {
-	geosimd::vec_container<std::array<my_vector, 3>> a;
+	geosimd::vec_container<my_vector, std::array<my_vector, 3>> a;
 	a[0] = my_vector{1, 2, 3, 4};
 	a[1] = my_vector{5, 6, 7, 8};
 	a[2] = my_vector{9, 10, 11, 12};
-	
-	geosimd::vec_container<std::list<my_vector>> b;
+
+	geosimd::vec_container<my_vector, std::list<my_vector>> b;
 	b.push_back(my_vector{12, 11, 10, 9});
 	b.push_back(my_vector{8, 7, 6, 5});
 	b.push_back(my_vector{4, 3, 2, 1});
-	
-	geosimd::vec_container<std::array<my_vector, 3>> expected_sum;
+
+	geosimd::vec_container<my_vector, std::array<my_vector, 3>> expected_sum;
 	expected_sum[2] = my_vector{13, 13, 13, 13};
 	expected_sum[1] = my_vector{13, 13, 13, 13};
 	expected_sum[0] = my_vector{13, 13, 13, 13};
-	
+
 	EXPECT_EQ(a + b, expected_sum);
 	EXPECT_EQ(b + a, expected_sum);
 }
 
 TESTCASE(geosimd_vec_container_mul_div)
 {
-	geosimd::vec_container<std::array<my_vector, 3>> a;
+	geosimd::vec_container<my_vector, std::array<my_vector, 3>> a;
 	a[0] = my_vector{1, 2, 3, 4};
 	a[1] = my_vector{5, 6, 7, 8};
 	a[2] = my_vector{9, 10, 11, 12};
-		
+
 	auto const old_a = a;
-	
+
 	a *= 3;
-	
-	geosimd::vec_container<std::array<my_vector, 3>> expected_prod;
+
+	geosimd::vec_container<my_vector, std::array<my_vector, 3>> expected_prod;
 	expected_prod[0] = 3*old_a[0];
 	expected_prod[1] = 3*old_a[1];
 	expected_prod[2] = 3*old_a[2];
-	
+
 	EXPECT_EQ(a, expected_prod);
-	
+
 	a/=3;
-	
+
 	EXPECT_EQ(a, old_a);
-	
+
 	auto const c = a*3;
 	EXPECT_EQ(c, expected_prod);
 	EXPECT_EQ(a,old_a);
