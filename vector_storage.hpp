064f13737fe7011f7e9a90054f16047f13c90228
Torbjörn Rathsman <milasudril@gmail.com>
2023-02-19 11:14:57 +0100

    Fix shuffle for 64-bit size

--- vector_storage.hpp
+++ vector_storage.hpp
@@ -85,13 +85,25 @@ namespace geosimd
 	}
 
 	template<class T, size_t N, class ... Indices>
-	requires (std::conjunction_v<std::is_same<int, Indices>...>
+	requires (sizeof(T) == 4
+		&& std::conjunction_v<std::is_same<int, Indices>...>
 		&& sizeof...(Indices) == N
 		&& vector_limits::can_vectorize<T>(N))
 	GEOSIMD_INLINE_OPT constexpr auto shuffle(vector_storage<T, N> a,
 		Indices ... vals)
 	{
 		return __builtin_shuffle(a, vector_storage<int, N>{vals...});
+	}
+
+	template<class T, size_t N, class ... Indices>
+	requires (sizeof(T) == 8
+		&& std::conjunction_v<std::is_same<int, Indices>...>
+		&& sizeof...(Indices) == N
+		&& vector_limits::can_vectorize<T>(N))
+	GEOSIMD_INLINE_OPT constexpr auto shuffle(vector_storage<T, N> a,
+		Indices ... vals)
+	{
+		return __builtin_shuffle(a, vector_storage<int64_t, N>{vals...});
 	}
 }
 
